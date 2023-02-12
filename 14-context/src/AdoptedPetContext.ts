1f417cef9320ad0324f78fb0e2dd2dac01a46cd6
Samad Arshad <samad.arshad@ev.energy>
2023-02-12 08:05:33 +0000

    ts1

---
+++ 14-context/src/AdoptedPetContext.ts
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+import { Pet } from "./APIResponsesTypes";
+
+const AdoptedPetContext = createContext<[Pet, (adoptedPet: Pet) => void]>([
+  {
+    id: 0,
+    name: "a",
+    animal: "dog",
+    description: "a",
+    breed: "a",
+    images: [],
+    city: "a",
+    state: "a",
+  },
+  () => {},
+]);
+
+export default AdoptedPetContext;
