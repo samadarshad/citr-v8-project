6686ab28545106cd438bf4dc0b18c03c92f9ec53
Samad Arshad <samad.arshad@ev.energy>
2023-02-14 08:08:49 +0000

    search params

--- 14-context/src/store.js
+++ 14-context/src/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import adoptedPet from "./adoptedPetSlice";
+import searchParams from "./searchParamsSlice";
 const store = configureStore({
-  reducer: { adoptedPet },
+  reducer: { adoptedPet, searchParams },
 });
 
 export default store;
