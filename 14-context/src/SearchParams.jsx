1fda3f8c191dde8abef2241a66c267c4efdd175b
Samad Arshad <samad.arshad@ev.energy>
2023-02-15 07:15:57 +0000

    Update API calls to use React Query and add search query
    : Update API calls to use React Query and add search query to fetch pet details and breed list, and search for pets using query parameters.

--- 14-context/src/SearchParams.jsx
+++ 14-context/src/SearchParams.jsx
@@ -1,21 +1,20 @@
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useQuery } from "@tanstack/react-query";
+import { useSearchQuery } from "./petApiService";
 import Results from "./Results";
 import useBreedList from "./useBreedList";
-import fetchSearch from "./fetchSearch";
 import { all } from "./searchParamsSlice";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const adoptedPet = useSelector((state) => state.adoptedPet.value);
-  const requestParams = useSelector((state) => state.searchParams.value);
+  const searchParams = useSelector((state) => state.searchParams.value);
   const [animal, setAnimal] = useState("");
   const [breeds] = useBreedList(animal);
   const dispatch = useDispatch();
 
-  const results = useQuery(["search", requestParams], fetchSearch);
-  const pets = results?.data?.pets ?? [];
+  let { data: pets } = useSearchQuery(searchParams);
+  pets = pets ?? [];
 
   return (
     <div className="search-params">
