21d2defe5ef42ffbfc67152ed00b53133214599d
Samad Arshad <samad.arshad@ev.energy>
2023-02-15 07:03:32 +0000

    Add petApi to store and configure store with petApi middleware

--- 14-context/src/store.js
+++ 14-context/src/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import adoptedPet from "./adoptedPetSlice";
 import searchParams from "./searchParamsSlice";
+import { petApi } from "./petApiService";
 const store = configureStore({
-  reducer: { adoptedPet, searchParams },
+  reducer: { adoptedPet, searchParams, [petApi.reducerPath]: petApi.reducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(petApi.middleware),
 });
 
 export default store;
